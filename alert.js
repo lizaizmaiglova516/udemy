df43c8943782d40c0a9b562e64cf68455dc2c826
Liza <lizaizmaiglova516@gmail.com>
2021-03-23 12:43:10 +0300

    first commit

---
+++ alert.js
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <body>
+      <script> alert("Я JavaScript!") 
+    </script>
+  </body>
+  </head>
+  </html>
---
+++ hw func.js
@@ -0,0 +1,4 @@
+
+let n;
+n=Math.floor(Math.random(n) * 10)+1;
+console.log(n);
