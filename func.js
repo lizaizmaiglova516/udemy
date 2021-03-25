b527f537a600bb73d71ac05b554e28674cfccd43
Liza <lizaizmaiglova516@gmail.com>
2021-03-25 17:06:28 +0300

    exercise 3

---
+++ func.js
@@ -0,0 +1,19 @@
+let num = 20;
+
+function showFirstMessage (text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage ("Hellow, World!");
+console.log(num);
+
+
+//второй пример
+function calc(a,b) {
+    return (a + b);
+}
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(10,6));
